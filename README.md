0806e6b6ed95db2b0e945e3ff89f5b31e167d6d6
Антон Пегов <antonpegov@gmail.com>
2018-02-11 18:36:10 +0200

    Setup initial infrastructure

---
+++ README.md
@@ -0,0 +1,21 @@
+# Simple Adress Book with Jason-Server fake beckend
+
+
+## Install
+
+Install json-sever and lite-server globally:
+```bash
+$ npm install -g json-server light-server
+```
+
+Install dependences:
+
+```bash
+$ npm install
+```
+
+Start application (you need posts 3000 and 4000 to be available):
+
+```bash
+npm start
+```
---
+++ data.js
@@ -0,0 +1,14 @@
+module.exports = function(){
+  const faker = require('faker');
+  const people = [];
+  for (let n = 0; n < 100; n+=1) {
+    people.push({
+      id: n,
+      fname: faker.name.firstName(),
+      lname: faker.name.lastName(),
+      avatar: faker.internet.avatar(),
+      phone: faker.phone.phoneNumber(),      
+      adress: `${faker.address.city()} ${faker.address.streetAddress()}`
+    })
+  }
+}
